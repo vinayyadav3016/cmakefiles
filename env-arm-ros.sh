93318cfae4cbc3d1fe874fab19b9294d16127257
vinayyadav3016 <vinayyadav3016@gmail.com>
2014-12-22 00:54:30 +0530

    Added arm-ros environment file

---
+++ env-arm-ros.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export ROS_ROOT=/home/vinay/ros_catkin_ws/install_isolated/share/ros
+export ROS_PACKAGE_PATH=/home/vinay/ros_catkin_ws/install_isolated/share:/home/vinay/ros_catkin_ws/install_isolated/stacks:/home/vinay/projects/ros-projects-arm
+export ROS_WORKSPACE=/home/vinay/projects/ros-projects-arm
+export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/:/home/vinay/ros_catkin_ws/install_isolated/lib/arm/arm-linux-gnueabihf:/home/vinay/ros_catkin_ws/install_isolated/lib
+export CPATH=/home/vinay/ros_catkin_ws/install_isolated/include
+export PATH=/opt/ros/indigo/bin:/home/vinay/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin:/home/vinay/android-sdk/tools/:/home/vinay/android-sdk/platform-tools/:/home/vinay/packages/eclipse/
+export ROSLISP_PACKAGE_DIRECTORIES=
+export ROS_DISTRO=indigo
+export PYTHONPATH=/home/vinay/ros_catkin_ws/install_isolated/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages
+export PKG_CONFIG_PATH=/home/vinay/ros_catkin_ws/install_isolated/lib/arm-linux-gnueabihf/pkgconfig:/home/vinay/ros_catkin_ws/install_isolated/lib/pkgconfig
+export CMAKE_PREFIX_PATH=/home/vinay/ros_catkin_ws/install_isolated
+export ROS_ETC_DIR=/home/vinay/ros_catkin_ws/install_isolated/etc/ros
